c72d01493cdd70ec402e1de016134b3bfd8b5e6a
Ryan Henness <ryanhenness@gmail.com>
2018-01-20 20:17:06 -0800

    Hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 5000);
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
