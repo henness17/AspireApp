c88d910db1532044f07ee3fc053af9d042dde828
Ryan Henness <ryanhenness@gmail.com>
2018-01-23 19:04:40 -0800

    Added check for logged in

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// The passport module Facebook strategy routes
+require('./passport.js')(app);
+
 // Set environment variables
 // If they are defined, then the app knows it's running on
 // Heroku, and not locally
@@ -16,12 +19,10 @@ if(process.env.PASSPORT_CLIENT_ID == undefined){
 }
 
 // Application root route page, aspireapp.herokuapp.com/
-app.get('/', function(req, res){
+app.get('/', loggedIn, function(req, res){
 	res.send('Hello World!');
 });
 
-// The passport module Facebook strategy routes
-require('./passport.js')(app);
 app.get('/login', function(req, res){
 	res.render('login', {user: req.user});
 });
@@ -29,3 +30,13 @@ app.get('/login', function(req, res){
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function loggedIn(req, res, next) {
+  if (req.user) {
+  	console.log(req.user);
+    next();
+  } else {
+  	console.log(req.user);
+    res.redirect('/login');
+  }
+}
